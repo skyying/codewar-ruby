74ae6dc86ba39748a4b0964def7fd63c7a3befa0
skyying <dreamuk@gmail.com>
2019-01-20 00:00:39 +0800

    add solutions

--- 3_Longest_Substring_Without_Repeating_Characters.rb
+++ 3_Longest_Substring_Without_Repeating_Characters.rb
@@ -22,3 +22,31 @@ end
 
 
 
+
+# solution 2
+def length_of_longest_substring(s)
+  # use array to store if repeats
+  table = Array.new(128, 0)
+  max = 0
+  start = 0
+  i = 0
+  # loop through every string
+  while i < s.size do
+    # get Unicode of current char
+    code = s[i].ord
+    # check if current char is already stored in the array
+    # if yes, get its index
+    # if not, keep previous start index, cause table[code] will be zero
+    start = [table[code], start].max
+
+    # update current max value
+    # + 1 means counts current char
+    max = [max, i - start + 1].max
+    # update index
+    table[code] = i + 1
+    # increase i 
+    i += 1
+  end
+  max
+end
+
