8cbfcdc62766a0e68d552dd9c533fe66b9565850
skyying <dreamuk@gmail.com>
2019-01-31 23:55:29 +0800

    add solutions

---
+++ tick_toward.rb
@@ -0,0 +1,53 @@
+
+
+# solution 1
+def tick_toward(start, target)
+  ans = [start]
+  current_x = start[0]
+  current_y = start[1]
+  end_x = target[0]
+  end_y = target[1]
+  while current_x != end_x or current_y != end_y
+        if current_x != end_x
+          if current_x > end_x
+            current_x -= 1
+          else
+            current_x += 1
+          end
+        end
+        
+        if current_y != end_y
+          if current_y > end_y
+            current_y -= 1
+          else
+            current_y += 1
+          end
+        end
+        ans.push [current_x, current_y]
+  end
+  ans
+end
+
+
+
+# solution 2
+def tick_toward(start, target)
+  result = [start]
+  next_target = start
+  until next_target == target do
+    next_target = next_target.map.with_index { |x, i|  x > target[i] ? x - 1 : x < target[i] ? x + 1 : x  }
+    result.push next_target
+  end
+  result
+end
+
+
+
+# solution 3
+def tick_toward(start, target)
+  return [start] if start == target
+
+  deltas = start.zip(target).map { |a, b| b <=> a }
+  tick = start.zip(deltas).map { |a, b| a + b }
+  [start] + tick_toward(tick, target)
+end
