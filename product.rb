84ba998485415e58084a2e1db93b18352427604c
Chien Wen Liu <dreamuk@gmail.com>
2019-03-31 16:18:28 +0800

    ruby product algorithm practice

--- product.rb
+++ product.rb
@@ -1,4 +1,5 @@
 
+# Implementation of ruby product algorithm
 class ArrayProduct
   def initialize(a)
     @a = a
@@ -6,8 +7,11 @@ class ArrayProduct
 
   def get_combination(*arrs)
     results = []
+
     ary = [@a] + arrs
     n = ary.size
+
+    # create a new array to record element we alreay looped
     cur_idx = Array.new(n, 0)
 
     while true
@@ -16,12 +20,20 @@ class ArrayProduct
       results << cur
 
       right = n - 1
+
+      # find the index in ary that we need to increment.
+      # start from right most sub-array
       while right >= 0 && (cur_idx[right] + 1 == ary[right].size)
         right -= 1 
       end
+
+      # all is looped
       return results if right < 0
 
+      # if right is not reach the boundary, increase the index in current right
       cur_idx[right] += 1
+
+      # reset subarray's index in cur_idx
       cur_idx = cur_idx.map.with_index { |el, i| i > right ? 0 : el }
     end
   end
