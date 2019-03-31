9b80ef9d5697092c1fdc830347d48873d73aa2a9
Chien Wen Liu <dreamuk@gmail.com>
2019-03-31 16:10:06 +0800

    ruby product algorithm practice

---
+++ product.rb
@@ -0,0 +1,31 @@
+
+class ArrayProduct
+  def initialize(a)
+    @a = a
+  end
+
+  def get_combination(*arrs)
+    results = []
+    ary = [@a] + arrs
+    n = ary.size
+    cur_idx = Array.new(n, 0)
+
+    while true
+      cur = []
+      (0...n).each { |i| cur << ary[i][cur_idx[i]] }
+      results << cur
+
+      right = n - 1
+      while right >= 0 && (cur_idx[right] + 1 == ary[right].size)
+        right -= 1 
+      end
+      return results if right < 0
+
+      cur_idx[right] += 1
+      cur_idx = cur_idx.map.with_index { |el, i| i > right ? 0 : el }
+    end
+  end
+end
+
+p l = ArrayProduct.new([1, 2])
+p l.get_combination(["a", "b", "c"], [10, 11])
